dd3ebc5b5c6d5491cc39d0b19de2db65e8e9a4b1
ConnelBLAZE <connelblaze@gmail.com>
2020-06-07 15:29:01 +0100

    update email

--- scripts/00534-connelblaze.py
+++ scripts/00534-connelblaze.py
@@ -1,8 +1,9 @@
 fullname = "Connel Evalsam Asikong"
 hng_id = "00534"
 lang = "Python"
+email = "connelblaze@gmail.com"
 
 def hello():
-    print("Hello World, this is " +fullname+ " with HNGi7 ID " +hng_id+ " using " +lang+ " for stage 2 task")
+    print("Hello World, this is " +fullname+ " with HNGi7 ID " +hng_id+ " using " +lang+ " for stage 2 task" +email)
 
 hello()
